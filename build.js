c062d913dd28a9b2fb962b10d18ab4b9d5e11214
Thiago Santos <thia.mdossantos@gmail.com>
2018-06-21 13:46:58 -0300

    feat: add sitemap

--- build.js
+++ build.js
@@ -6,6 +6,7 @@ const Metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
 const layouts = require('metalsmith-layouts')
 const permalinks = require('metalsmith-permalinks')
+const sitemap = require('metalsmith-sitemap')
 
 function getMetadata() {
   return yaml.safeLoad(
@@ -45,6 +46,11 @@ Metalsmith(__dirname)
   .use(markdown())
   .use(permalinks())
   .use(layouts())
+  .use(
+    sitemap({
+      hostname: 'https://brainn.co/'
+    })
+  )
   .build(function(err, files) {
     if (err) {
       throw err
